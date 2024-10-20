f6bcbbf9bb5989f18b8a2c35d3b0813d5ad6dcb0
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-10-20 11:44:42 +0300

    -> Added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Leva } from "leva";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
     <Leva collapsed />
+    <Analytics />
   </StrictMode>
 );
